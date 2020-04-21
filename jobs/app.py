42cf1735af2129727b4f5ce83e6f2b12bdbcedf3
jkeyser <jake.keyser@colostate.edu>
2020-04-21 14:21:21 -0600

    fixed syntax errors

--- jobs/app.py
+++ jobs/app.py
@@ -1,27 +1,29 @@
 from flask import Flask, render_template, g
 import sqlite3
 
-PATH="db/jobs.sqlite"
+PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
 
+
 def open_connection():
-   connection =  getattr(g, "_connection", None)
-   if connection is None:
-       connection, g._connection = sqlite3.connect(PATH)
-   connection.row_factory = sqlite3.Row
-   
-   return connection
+    connection = getattr(g, "_connection", None)
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    cursor = execute(connection(sql, values))
+    cursor = connection.execute(sql, values)
     if commit is True:
         results = connection.commit()
     else:
-        results = if: cursor.fetchone() if single else cursor.fetchall()
-    
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
     return results
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -29,7 +31,8 @@ def close_connection(exception):
     if connection is not None:
         connection.close()
 
+
 @app.route("/jobs")
 @app.route("/")
 def jobs():
-    return render_template('index.html')
+    return render_template("index.html")
